b49464c52e63753118195dc77ca9168be74c6bd5
houzi- <wojiaolinmu008@gmail.com>
2020-09-17 22:36:21 +0800

    Upload javascript <youtube_skip_ad.js>

---
+++ 5.0/youtube_skip_ad.js
@@ -0,0 +1,39 @@
+        var css = '.video-ads .ad-container .adDisplay,#player-ads,.ytp-ad-module,.ytp-ad-image-overlay{ display: none!important; }',
+            head = document.head || document.getElementsByTagName('head')[0],
+            style = document.createElement('style');
+
+        style.type = 'text/css';
+        if (style.styleSheet){
+            style.styleSheet.cssText = css;
+        } else {
+            style.appendChild(document.createTextNode(css));
+        }
+
+        head.appendChild(style);
+    };
+    var skipInt;
+    var log=function(msg){
+       // unsafeWindow.console.log (msg);
+    };
+    var skipAd=function(){
+        //ytp-ad-preview-text
+        //ytp-ad-skip-button
+        var skipbtn=document.querySelector(".ytp-ad-skip-button.ytp-button")||document.querySelector(".videoAdUiSkipButton ");
+        //var skipbtn=document.querySelector(".ytp-ad-skip-button ")||document.querySelector(".videoAdUiSkipButton ");
+        if(skipbtn){
+           skipbtn=document.querySelector(".ytp-ad-skip-button.ytp-button")||document.querySelector(".videoAdUiSkipButton ");
+           log("skip");
+           skipbtn.click();
+           if(skipInt) {clearTimeout(skipInt);}
+           skipInt=setTimeout(skipAd,500);
+         }else{
+              log("checking...");
+              if(skipInt) {clearTimeout(skipInt);}
+              skipInt=setTimeout(skipAd,500);
+         }
+    };
+
+    closeAd();
+    skipAd();
+
+})();
