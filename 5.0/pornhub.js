f4169476ee4998caa10935a1fabf821cf3001dc5
houzi <wojiaolinmu008@gmail.com>
2020-11-29 20:18:34 +0800

    Upload pornhub.js

---
+++ 5.0/pornhub.js
@@ -0,0 +1,4 @@
+window.opera = true;
+Object.defineProperty(window,'tj_ads',{get:function(){return[]}});
+Object.defineProperty(Object.prototype, 'loadPopUnder', { get: function(){ return function() {}; }, set: function(){ return function() {}; }});
+Object.defineProperty(window, 'UAParser', { get: function() { return function() { }; } });
