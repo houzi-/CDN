d4cc1cf01d6ef45be2a63dd80b74a032c9b62f4d
houzi <wojiaolinmu008@gmail.com>
2022-02-06 11:49:32 +0800

    Add files via upload

---
+++ KP-/test.js
@@ -0,0 +1,1 @@
+fuckcss('#masthead-ad,[class*="ytd-promoted-sparkles-text-search-renderer"],.ytd-promoted-sparkles-text-search-renderer,.ytd-carousel-ad-renderer,.badge-style-type-ad,[layout*="display-ad-"],.ytp-ad-progress-list,#merch-shelf,#player-ads,#offer-module')
