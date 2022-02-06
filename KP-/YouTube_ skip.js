70570c82c2df30f33ef5e20c79ecb77166c057e5
houzi <wojiaolinmu008@gmail.com>
2022-02-06 13:22:06 +0800

    Update YouTube_ skip.js

--- KP-/YouTube_ skip.js
+++ KP-/YouTube_ skip.js
@@ -1,15 +1,5 @@
 var WL_YTASP = true;
 
-var pbs = document.createElement("INPUT");
-pbs.type="number";
-pbs.style="background-color: black;color: white;background-repeat:no-repeat;border: none;cursor:pointer;overflow: hidden;outline:none;/*-moz-appearance: textfield;*/width:8vw;text-align: center;font-size:auto;/*pointer-events: none;*/";
-pbs.step=0.1;
-pbs.min=0;
-pbs.value=1.0;
-pbs.addEventListener("change",function(){if(pbs.value>0){document.getElementsByTagName("video")[0].playbackRate = pbs.value;}else{pbs.value=1;document.getElementsByTagName("video")[0].playbackRate = pbs.value;}});
-
-document.getElementById('center').appendChild(pbs);
-
 const observer = new MutationObserver((mutations) => {
   mutations.forEach((mutation) => {
       if (document.contains(document.getElementsByClassName('ytp-ad-skip-button ytp-button')[0])) {
